879428699f4e846f352803be1665d1e65dff6399
Diego Celestino <diegocdsantos@gmail.com>
2022-06-02 18:24:20 -0300

    Commit do item 3 - Create a Feature Component do projeto Tour of Heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,13 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 12, name: 'Super Tango' },
+  { id: 13, name: 'Cristiano Ronaldo' },
+  { id: 14, name: 'Rafinha do BBB' },
+  { id: 15, name: 'Magnético' },
+  { id: 16, name: 'Homem Bala de Borracha' },
+  { id: 17, name: 'Fábio Porchat' },
+  { id: 18, name: 'Al khe Buho' },
+  { id: 19, name: 'Quentão' },
+  { id: 20, name: 'Luca Modric Ao Contrário' }
+];
